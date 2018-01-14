318e985a4c05a0dfa3025f1a16483daea98c08ae
FlorianH123 <f.hennrich@gmx.de>
2018-01-14 20:29:15 +0100

    init

---
+++ app/src/main/java/com/mobileapplicationdev/downloadservice/DownloadService.java
@@ -0,0 +1,97 @@
+package com.mobileapplicationdev.downloadservice;
+
+import android.app.ActivityManager;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Florian on 14.01.2018.
+ * DownloadService
+ */
+
+public class DownloadService extends Service {
+    private final IBinder mBinder = new LocalBinder();
+    private double currentProgress;
+
+    class LocalBinder extends Binder {
+        DownloadService getService() {
+            return DownloadService.this;
+        }
+    }
+
+    @Override
+    public int onStartCommand(final Intent intent, int flags, int startId) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                stopSelf();
+            }
+        }, "my.thread.name").start();
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+    @Override
+    public void onCreate() {
+
+    }
+
+    public void startDownload(final String downloadUrl) {
+        currentProgress = 0.0;
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long fileSize;
+                int bytesRead;
+                int totalBytesRead = 0;
+                double currentProgress;
+
+                try {
+                    URL url = new URL(downloadUrl);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+                    fileSize = conn.getContentLength();
+                    BufferedInputStream in = new BufferedInputStream(conn.getInputStream());
+
+                    byte[] data = new byte[1024];
+
+                    while ((bytesRead = in.read(data, 0, 1024)) >= 0) {
+                        totalBytesRead += bytesRead;
+
+                        currentProgress = ((((double)totalBytesRead) / ((double)fileSize))) * 100;
+                        setCurrentProgress(currentProgress);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                stopSelf();
+            }
+        }, "my.thread.name").start();
+    }
+
+    public void setCurrentProgress(double currentProgress) {
+        this.currentProgress = currentProgress;
+    }
+
+    public double getCurrentProgress() {
+        return currentProgress;
+    }
+}
---
+++ app/src/main/java/com/mobileapplicationdev/downloadservice/MainActivity.java
@@ -0,0 +1,107 @@
+package com.mobileapplicationdev.downloadservice;
+
+import android.app.ActivityManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Handler;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private ProgressBar progressBar;
+    private TextView textView;
+
+    private DownloadService downloadService;
+    private boolean mBound = false;
+    private int progressStatus = 0;
+    private Handler handler = new Handler();
+
+    private ServiceConnection mConn = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            DownloadService.LocalBinder binder = (DownloadService.LocalBinder) service;
+            downloadService = binder.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mBound = false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.url_text);
+        progressBar = findViewById(R.id.progressBar);
+        Button button = findViewById(R.id.button);
+
+        Intent intent = new Intent(this, DownloadService.class);
+
+        if (!isServiceRunning(DownloadService.class)) {
+            startService(intent);
+            bindService(intent, mConn, Context.BIND_AUTO_CREATE);
+        } else {
+            bindService(intent, mConn, Context.BIND_AUTO_CREATE);
+            textView.setActivated(false);
+            button.setActivated(false);
+            updateProgressBar();
+        }
+
+
+    }
+
+    public void onClick(View view) throws InterruptedException {
+        String text = textView.getText().toString();
+
+        //text = "http://emrolab.htw-saarland.de/android/wp-content/uploads/2018/01/WS17-MADA-10-ContentProvider.pdf";
+
+        if (text.equals("")) {
+            Toast.makeText(MainActivity.this, R.string.no_url_exception, Toast.LENGTH_SHORT).show();
+        } else {
+            if (mBound) {
+                downloadService.startDownload(text);
+                updateProgressBar();
+            }
+        }
+    }
+
+    public void updateProgressBar() {
+        progressStatus = 0;
+
+        new Thread(new Runnable() {
+            public void run() {
+                while (progressStatus < 100) {
+                    progressStatus = (int) downloadService.getCurrentProgress();
+
+                    handler.post(new Runnable() {
+                        public void run() {
+                            progressBar.setProgress(progressStatus);
+                        }
+                    });
+                }
+            }
+        }).start();
+    }
+
+    private boolean isServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)){
+            if(serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
